cec17a0ef42ae3916360f86b11cf8605beb23180
Kirill <akvamarin0610@gmail.com>
2022-04-25 20:00:32 +0400

    js

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const form = document.getElementById('res');
+"use strict"
 
+    const form = document.getElementById('res');
+    var values = {
+
+};
+
+function showPT(table, place) {
+    tShow = document.querySelector("#el1");
+    pShow = document.querySelector('#el2');
+
+    if (localStorage.getItem('table') !== undefined && localStorage.getItem('place') !== undefined) {
+
+    }
+
+}
 function retrieveFormValue(event) {
     event.preventDefault();
-    const table = form.querySelector('[name="table"]'),
-        place = form.querySelector('[name="place"]');
+    let table = form.querySelector('[name="table"]');
+    let  place = form.querySelector('[name="place"]');
 
-    const values = {
-        table: table.value,
-        place: place.value
-    };
+    values["table"] = table.value;
+    values["place"] = place.value;
     console.log('', values);
+    localStorage.setItem('table', values["table"]);
+    localStorage.setItem('place', values["place"]);
+
+
 }
 
 form.addEventListener('submit', retrieveFormValue);
+
+
+
+console.log(localStorage.getItem('table'));
+console.log(localStorage.getItem('place'));
+
